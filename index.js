cf0f082662d47ed83aaa7c4a02bd2ddec66006e4
Mymuna Murshed <118626215+Niama001@users.noreply.github.com>
2023-08-28 00:44:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -149,8 +149,12 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+const [firstGame, secondGame] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const firstGameElement = document.createElement("p");
+firstGameElement.textContent = `${firstGame.name}`;
+firstGameElement.appendChild(firstGameElement);
 
 // do the same for the runner up item
 const secondGameElement = document.createElement("p");
